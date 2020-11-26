5b6cf6c930a648f7b100993d707abd6c325f8547
Simo Tukiainen <simo.tukiainen@fmi.fi>
2020-11-26 12:54:54 +0200

    Add DELETE /cloudnet-product-volatile/

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,14 @@ import config from './config'
 
 export class Middleware {
 
-  validateParams: RequestHandler = (req, res, next) => {
+  validateDeleteBucket: RequestHandler = (req, _, next) => {
+    const bucket = req.params.bucket
+    const allowedBuckets = ['cloudnet-product-volatile', 'test']
+    if (!allowedBuckets.includes(bucket)) return next({status: 405, msg: 'DELETE not allowed for the bucket'})
+    return next()
+  }
+
+  validateParams: RequestHandler = (req, _, next) => {
     const bucket = req.params.bucket
     if (!config.buckets.includes(bucket)) return next({status: 404, msg: 'Unknown bucket'})
     req.params.key = req.params[0]
