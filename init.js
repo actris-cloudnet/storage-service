8d87b2139d4d114c4bcce3b1e013ecd82a0862e9
Lauri Kangassalo <lauri.kangassalo@fmi.fi>
2021-03-05 14:18:10 +0200

    Create tables

--- init.js
+++ init.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
 const AWS = require('aws-sdk')
+const Client = require('pg').Client
 
 const s3 = new AWS.S3(JSON.parse(fs.readFileSync('src/config/local.connection.json').toString()))
 
 const buckets = ['cloudnet-test-volatile', 'cloudnet-test-versioning', 'cloudnet-test-versioning-1'];
 
 (async () => {
+  if (process.env.NODE_ENV == 'production' || process.env.SS_MODE != 'local') {
+    console.log('Not running in local mode. Skipping bucket and table creation.')
+    return
+  }
+
   process.stdout.write('Initializing buckets... ')
   try {
     await Promise.all(buckets.map(bucket =>
@@ -21,4 +27,13 @@ const buckets = ['cloudnet-test-volatile', 'cloudnet-test-versioning', 'cloudnet
   }
   await s3.putBucketVersioning(params).promise()
   console.log('OK')
+
+  process.stdout.write('Initializing db... ')
+  const client = new Client()
+  await client.connect()
+  await client.query(fs.readFileSync('src/db/createtable.sql', 'utf-8'))
+  await client.query(fs.readFileSync('src/db/createtable-test.sql', 'utf-8'))
+  console.log('OK')
+
+  process.exit(0)
 })()
